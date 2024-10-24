f33c8c733c670c904e55fa2d55e4e13b1e6b518a
pageyboy <chris_pagey100@hotmail.com>
2024-10-24 16:20:50 +0100

    Added minimum working example

---
+++ Example.Tests/BaseTestClass.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Example.Tests
+{
+    [TestClass]
+    [Ignore]
+    public class BaseTestClass
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.Fail();
+        }
+
+        [TestClass]
+        public class DerivedTestClass : BaseTestClass
+        {
+            [TestMethod]
+            public void TestMethod2()
+            {
+                Assert.Fail();
+            }
+        }
+    }
+}
---
+++ Example.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Example.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Example.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c6a93748-89c1-4a51-b239-cea919c7f7f3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
